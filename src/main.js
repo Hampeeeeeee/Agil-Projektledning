2df9ac91b59ac17299fa31b864325ef8602912ea
LarebKhalid <lareb.usman@yahoo.com>
2025-05-15 23:22:30 +0200

    homepage fixed
    homepage fixed

--- src/main.js
+++ src/main.js
@@ -38,3 +38,55 @@ async function displayProducts() {
 }
 
 displayProducts();
+const sliderImage = document.querySelector('.sliderimg');
+const sliderImages = document.querySelectorAll('.sliderimg img');
+
+// Buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+// Counter
+let counter = 1;
+const size = sliderImages[0].clientWidth;
+
+sliderImage.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+// Button Listeners
+nextBtn.addEventListener('click', () => {
+  if (counter >= sliderImages.length - 1) return;
+
+  sliderImage.style.transition = 'transform 0.4s ease-in-out';
+  counter++;
+  sliderImage.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+prevBtn.addEventListener('click', () => {
+  if (counter <= 0) return;
+
+  sliderImage.style.transition = 'transform 0.4s ease-in-out';
+  counter--;
+  sliderImage.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+sliderImage.addEventListener('transitionend', () => {
+  if (sliderImages[counter].id === 'lastclone') {
+    sliderImage.style.transition = 'none';
+    counter = sliderImages.length - 2;
+    sliderImage.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  }
+
+  if (sliderImages[counter].id === 'first-clone') {
+    sliderImage.style.transition = 'none';
+    counter = 1;
+    sliderImage.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  }
+});
+
+// Auto-slide every 3 seconds
+let autoSlide = setInterval(() => {
+  if (counter >= sliderImages.length - 1) return;
+
+  sliderImage.style.transition = 'transform 0.4s ease-in-out';
+  counter++;
+  sliderImage.style.transform = 'translateX(' + (-size * counter) + 'px)';
+}, 3000);
