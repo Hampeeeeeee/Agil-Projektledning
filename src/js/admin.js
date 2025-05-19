03b74018d5c8ef10e21c717874b3775d95af2195
munchkin870411 <munchkin870411@gmail.com>
2025-05-19 09:47:32 +0200

    postProduct

--- src/js/admin.js
+++ src/js/admin.js
@@ -20,3 +20,43 @@ export function populateCategoryDropdown() {
 document.addEventListener("DOMContentLoaded", () => {
     populateCategoryDropdown();
 });
+
+// Function to post pruduct from admin page to backend
+async function postProduct(product) {
+    try {
+        const response = await fetch("http://localhost:3000/products", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(product),
+        });
+
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        console.log("Product added:", data);
+    } catch (error) {
+        console.error("Error adding product:", error);
+    }
+}
+
+const productForm = document.getElementById("admin-form");
+productForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const product = {
+        name: document.getElementById("product-name").value,
+        brand: document.getElementById("product-brand").value,
+        category: document.getElementById("product-category").value,
+        department: document.getElementById("product-department").value,
+        price: parseFloat(document.getElementById("product-price").value),
+        image1: document.getElementById("product-image1").value,
+        image2: document.getElementById("product-image2").value,
+        image3: document.getElementById("product-image3").value,
+    };
+
+    postProduct(product);
+});
